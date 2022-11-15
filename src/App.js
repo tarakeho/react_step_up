d535469d44cc979144b4566e57406d6a94ab6c8d
tarakeho <speedy@aqu.bekkoame.ne.jp>
2022-11-15 20:05:44 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+}
